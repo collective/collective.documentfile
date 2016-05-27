e934ac8f6c45224cb772070bbbc03f86ffba8286
Petri Savolainen <petri.savolainen@koodaamo.fi>
2016-05-27 14:02:47 +0300

    Prepare collective.documentfile 1.0rc0.

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,6 +1,13 @@
 Changelog
 =========
 
+1.0rc0 (unreleased)
+-------------------
+
+- Don't break when behavior is set on content type without image field
+  [petri]
+- Other minor fixes
+  [petri]
 
 1.0a1 (unreleased)
 ------------------
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.documentfile',
-    version='1.0a2',
+    version='1.0rc0',
     description="Document file (MS Office etc.)",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
